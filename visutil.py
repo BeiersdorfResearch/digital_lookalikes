c707582bc62a1c930c0f89a891e905af50000edb
Fayez Bajjali <fayez.bajjali@beiersdorf.com>
2023-06-20 13:49:09 +0000

    Change color of landmark drawing

--- visutil.py
+++ visutil.py
@@ -22,12 +22,16 @@ def draw_landmarks_on_image(rgb_image, detection_result):
             ]
         )
 
+        mp.solutions.drawing_styles.get_default_face_mesh_tesselation_style()
+
         solutions.drawing_utils.draw_landmarks(
             image=annotated_image,
             landmark_list=face_landmarks_proto,
             connections=mp.solutions.face_mesh.FACEMESH_TESSELATION,
             landmark_drawing_spec=None,
-            connection_drawing_spec=mp.solutions.drawing_styles.get_default_face_mesh_tesselation_style(),
+            connection_drawing_spec=mp.solutions.drawing_styles.DrawingSpec(
+                color=(0, 241, 241)
+            ),
         )
         solutions.drawing_utils.draw_landmarks(
             image=annotated_image,
