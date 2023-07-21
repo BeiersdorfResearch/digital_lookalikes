e117e21ffb79eca402d39d978eb931b82270490b
Fayez Bajjali <fayez.bajjali@beiersdorf.com>
2023-07-21 09:04:09 +0000

    Add nr_selfies and blob filtering

--- src/data/selfies.py
+++ src/data/selfies.py
@@ -4,7 +4,8 @@ from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
 
 import hydra
-import mediapipe as mp
+from PIL import Image
+import numpy as np
 import pandas as pd
 from azure.core.exceptions import ResourceNotFoundError
 from azure.identity import ManagedIdentityCredential
@@ -12,11 +13,10 @@ from azure.storage.blob import BlobClient
 from dl_conn import get_dl_conn
 from dl_orm import DLorm
 from omegaconf import DictConfig
+from tqdm import tqdm
 
 con = get_dl_conn()
 datalake = DLorm(con)
-
-credential = ManagedIdentityCredential()
 
 
 def get_user_selfie_data(cfg: DictConfig) -> pd.DataFrame:
@@ -26,7 +26,6 @@ def get_user_selfie_data(cfg: DictConfig) -> pd.DataFrame:
             ,pgsfe.id
             ,pgsfe.full_path
             ,pgsfe.selfie_link_id
-            ,u.nr_selfies
         FROM pg.selfie as pgsfe
         JOIN pg.users as u ON u.user_id = pgsfe.user_id
 
@@ -35,41 +34,38 @@ def get_user_selfie_data(cfg: DictConfig) -> pd.DataFrame:
         AND pgsfe.ts_date 
             BETWEEN '{cfg.dl_filters.earliest_ts_date}' AND '{cfg.dl_filters.latest_ts_date}'
         AND u.participant_type = '{cfg.dl_filters.participant_type}'
-        AND u.nr_selfies > {cfg.dl_filters.nr_selfies}
         AND pgsfe.selfie_link_id in (SELECT DISTINCT selfie_link_id FROM pg.measure_procedure)
     """
-    return datalake.get_query(query).sort_values("user_id")
+    df_selfies = datalake.get_query(query).sort_values("user_id")
+    filter_users = get_users_w_min_nrselfies(df_selfies, cfg.dl_filters.nr_selfies)
+    df_selfies = df_selfies.loc[df_selfies["user_id"].isin(filter_users)]
+    return df_selfies
 
 
-"""
-SELECT pgsfe.[user_id]
-        ,pgsfe.[ts_date]
-        ,pgsfe.[id]
-        ,pgsfe.[full_path]
-    FROM [pg].[selfie] as pgsfe
-    JOIN pg.users as u ON u.user_id = pgsfe.user_id
-    JOIN pg.measure_procedure as pgmp ON pgmp.user_id = pgsfe.user_id
+def get_users_w_min_nrselfies(
+    df_selfies: pd.DataFrame, min_nr_selfies: int
+) -> np.ndarray:
+    df_nr_selfies = (
+        df_selfies.groupby("user_id")
+        .nunique()
+        .reset_index()[["user_id", "selfie_link_id"]]
+        .rename(columns={"selfie_link_id": "nr_selfies"})
+    )
 
-    WHERE error_code IS NULL
-    AND pgsfe.anonymization_date IS NOT NULL
-    AND pgsfe.ts_date 
-        BETWEEN '2019-01-01' AND '2023-07-17'
-    AND u.participant_type = 'SKINLY'
-    AND pgsfe.selfie_link_id in (select distinct selfie_link_id from pg.measure_procedure)
-    AND pgsfe.user_id in (SELECT user_id
-            FROM pg.measure_procedure
-            GROUP BY user_id
-            HAVING COUNT(DISTINCT selfie_link_id) > 50) 
-"""
+    return np.array(
+        df_nr_selfies.loc[df_nr_selfies["nr_selfies"] >= min_nr_selfies][
+            "user_id"
+        ].values
+    )
 
 
 def init_blob(
-    credential: ManagedIdentityCredential,
     user_id: int,
     date: str,
     filename: str,
     container_name: str = "selfies",
 ):
+    credential = ManagedIdentityCredential()
     return BlobClient(
         account_url="https://claire1kstorage.blob.core.windows.net",
         container_name=container_name,
@@ -78,7 +74,56 @@ def init_blob(
     )
 
 
-def download_blob(blob, save_path: Path | str):
+def check_blob_exists(row):
+    path = Path(row["full_path"])
+    date = path.parts[3]
+    filename = path.parts[5]
+    user_id = row["user_id"]
+
+    blob = init_blob(user_id, date, filename)
+
+    return np.nan if blob.exists() else row["selfie_link_id"]
+
+
+def filter_bad_blobs(df_selfies: pd.DataFrame) -> pd.Series:
+    non_existent_blobs = []
+    with tqdm(total=len(df_selfies), ncols=100) as pbar:
+        with ThreadPoolExecutor(max_workers=12) as executor:
+            futures = [
+                executor.submit(check_blob_exists, row)
+                for row in df_selfies.to_dict("records")
+            ]
+            for future in concurrent.futures.as_completed(futures):
+                try:
+                    non_existent_blobs.append(future.result())
+                    pbar.update(1)
+                except Exception as e:
+                    pbar.update(1)
+                    print(f"{future} raised an exception {e}")
+    return pd.Series(non_existent_blobs).dropna()
+    # return df_selfies.loc[~df_selfies["selfie_link_id"].isin(non_existent_blobs)]
+
+
+def sample_user_selfies(df_selfies: pd.DataFrame) -> pd.DataFrame:
+    df_latest_selfie = df_selfies.sort_values("ts_date").groupby("user_id").tail(1)
+    df_random_selfies = (
+        df_selfies.loc[
+            ~df_selfies["selfie_link_id"].isin(df_latest_selfie["selfie_link_id"])
+        ]
+        .groupby("user_id")
+        .sample(2)
+    )
+    return pd.concat([df_latest_selfie, df_random_selfies]).sort_values("user_id")
+
+
+def download_blob(
+    user_id: int,
+    date: str,
+    filename: str,
+    save_path: Path | str,
+):
+    blob = init_blob(user_id, date, filename)
+
     save_path = Path(save_path)
     if save_path.exists():
         return
@@ -87,8 +132,8 @@ def download_blob(blob, save_path: Path | str):
         try:
             data = blob.download_blob()
             data.readinto(f)
-        except ResourceNotFoundError:
-            raise
+        except ResourceNotFoundError as e:
+            raise ResourceNotFoundError(f"Blob for {user_id}-{date} not found.") from e
 
 
 def get_selfie(dataframe_row: dict, save_dir: Path | str):
@@ -97,53 +142,60 @@ def get_selfie(dataframe_row: dict, save_dir: Path | str):
     filename = path.parts[5]
     user_id = dataframe_row["user_id"]
 
-    blob = init_blob(credential, user_id, date, filename)
-
     save_dir = Path(save_dir)
     save_dir = save_dir / Path(f"{user_id}")
     save_dir.mkdir(parents=True, exist_ok=True)
-    save_path = save_dir / Path(f"{date}.jpg")
+    save_path = save_dir / Path(f"{date}{dataframe_row['selfie_link_id']}.jpg")
 
     try:
-        download_blob(blob, save_path)
-        return save_path
-    except ResourceNotFoundError:
-        # print(f"Blob for {user_id}-{date} not found.")
-        return Exception(f"Blob for {user_id}-{date} not found.")
+        download_blob(
+            user_id=user_id,
+            date=date,
+            filename=filename,
+            save_path=save_path,
+        )
+    except ResourceNotFoundError as e:
+        raise ResourceNotFoundError(f"Blob for {user_id}-{date} not found.") from e
 
 
 def get_selfies(df_selfies: pd.DataFrame, save_dir: Path | str = "../../data/selfies"):
-    with ThreadPoolExecutor(max_workers=12) as executor:
-        futures = [
-            executor.submit(get_selfie, row, save_dir=save_dir)
-            for row in df_selfies.to_dict("records")
-        ]
-        for future in concurrent.futures.as_completed(futures):
-            try:
-                selfie_path = future.result()
-                print(selfie_path)
-            except Exception as e:
-                print(f"{future} raised an exception {e}")
-                continue
+    l = len(df_selfies)
+    with tqdm(total=l, ncols=100) as pbar:
+        with ThreadPoolExecutor(max_workers=12) as executor:
+            futures = [
+                executor.submit(get_selfie, row, save_dir=save_dir)
+                for row in df_selfies.to_dict("records")
+            ]
+            for future in concurrent.futures.as_completed(futures):
+                try:
+                    future.result()
+                    pbar.update(1)
+                except Exception as e:
+                    print(f"{future} raised an exception {e}")
+                    pbar.update(1)
 
 
 def validate_selfie(path: Path | str):
     path = Path(path)
     try:
-        mp.Image.create_from_file(path.as_posix())
-    except RuntimeError:
-        path.unlink()
+        img = Image.open(path.as_posix())
+        img.verify()
+    except (IOError, SyntaxError) as e:
+        raise
 
 
 def validate_selfies(paths: list[Path | str]):
-    with ThreadPoolExecutor(max_workers=12) as executor:
-        futures = [executor.submit(validate_selfie, path) for path in paths]
-        for future in concurrent.futures.as_completed(futures):
-            try:
-                selfie_path = future.result()
-            except Exception as e:
-                print(f"{future} raised an exception {e}")
-                continue
+    l = len(paths)
+    with tqdm(total=l, ncols=100) as pbar:
+        with ThreadPoolExecutor(max_workers=12) as executor:
+            futures = [executor.submit(validate_selfie, path) for path in paths]
+            for future in concurrent.futures.as_completed(futures):
+                try:
+                    future.result()
+                    pbar.update(1)
+                except Exception as e:
+                    print(f"{future} raised an exception {e}")
+                    pbar.update(1)
 
 
 # %%
@@ -151,44 +203,28 @@ with hydra.initialize(version_base=None, config_path="../../config"):
     cfg = hydra.compose(config_name="config")
     print(cfg.dl_filters)
 df_selfies = get_user_selfie_data(cfg)
-df_selfies
+# %%
+df_latest_selfie = df_selfies.sort_values("ts_date").groupby("user_id").tail(2)
+# %%
+bad_blobs_latest = filter_bad_blobs(df_latest_selfie)
+# %%
+bad_blobs = filter_bad_blobs(df_selfies)
 # %%
 
-df_selfienrcomp = (
-    df_selfies[["user_id", "id", "full_path", "selfie_link_id"]]
-    .groupby("user_id")
-    .nunique()
-    .reset_index()
-)
-df_selfienrcomp
 # %%
+df_sampled_selfies = sample_user_selfies(df_selfies)
+get_selfies(df_sampled_selfies)
+validate_selfies(list(Path("../../data/selfies").glob("**/*.jpg")))
+# %%
+# @hydra.main(config_path="../../config", config_name="config", version_base=None)
+# def main(cfg: DictConfig):
+#     df_selfies = get_user_selfie_data(cfg)
+#     df_sampled_selfies = sample_user_selfies(df_selfies)
+#     get_selfies(df_sampled_selfies)
+#     validate_selfies(list(Path("../../data/selfies").glob("**/*.jpg")))
 
 
-df_selfienrcomp = df_selfienrcomp.merge(
-    df_selfies[["user_id", "nr_selfies"]], on="user_id", how="left"
-).drop_duplicates()
-df_selfienrcomp
-# %%
-print(df_selfienrcomp.head(10).to_markdown())
-# %%
-df_grouped_nunique = df_selfies.groupby("selfie_link_id").nunique().reset_index()
-# %%
-df_grouped_nunique.loc[df_grouped_nunique["full_path"] > 1]
-# %%
-df_selfies.loc[df_selfies.duplicated("full_path")]
-# %%
+# if __name__ == "__main__":
+#     main()
 
-# paths = list(Path("./data/selfies/").rglob("*.jpg"))
-# paths = [path.as_posix() for path in paths]
-
-# corrupted_images = []
-# for path in tqdm(paths):
-#     try:
-#         mp.Image.create_from_file(path)
-#     except RuntimeError:
-#         corrupted_images.append(path)
-# # %%
-# pd.DataFrame(corrupted_images, columns=["path"]).to_csv(
-#     "./corrupted_files.csv", index=False
-# )
 # %%
